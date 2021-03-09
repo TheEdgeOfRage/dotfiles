1f7776d2b150d099aebbcf56167c960adf8a245c
Pavle Portic <pavle.portic@tilda.center>
2021-03-09 12:49:11 +0100

    Update rpn clone URL in init

--- init.sh
+++ init.sh
@@ -15,5 +15,5 @@ make neovim
 cd -
 
 git clone https://github.com/nikita-skobov/create-bash-script ~/.local/share/create-bash-script
-git clone https://gitlab.theedgeofrage.com/TheEdgeOfRage/rpn ~/.local/share/rpn
+git clone https://gitea.theedgeofrage.com/TheEdgeOfRage/rpn ~/.local/share/rpn
 git clone https://github.com/tobi-wan-kenobi/bumblebee-status ~/.local/share/bumblebee-status
