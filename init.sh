5189eb6e4ddd6f51a498fef3c9b5114a1f924755
Pavle Portic <git@theedgeofrage.com>
2021-10-10 13:00:28 +0200

    Change zsh-autoswitch-venv path

--- init.sh
+++ init.sh
@@ -14,7 +14,7 @@ mkdir -p ~/.config/zsh/plugins/
 git clone https://github.com/TheEdgeOfRage/ohmyzsh ~/.config/zsh/oh-my-zsh
 
 git clone https://github.com/MichaelAquilina/zsh-you-should-use ~/.config/zsh/plugins/you-should-use
-git clone https://github.com/TheEdgeOfRage/zsh-autoswitch-virtualenv ~/.config/zsh/plugins/autoswitch-virtualenv
+git clone https://github.com/TheEdgeOfRage/zsh-autoswitch-virtualenv ~/.config/zsh/plugins/zsh-autoswitch-virtualenv
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.config/zsh/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.config/zsh/plugins/zsh-syntax-highlighting
