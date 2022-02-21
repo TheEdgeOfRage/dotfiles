987b734e8fa189dabdeb7cb2b8c31a9ced79bfd3
Pavle Portic <git@theedgeofrage.com>
2022-02-21 22:57:49 +0100

    Move custom zsh plugins to ohmyzsh custom directory

--- init.sh
+++ init.sh
@@ -6,15 +6,15 @@ make vim
 make neovim
 cd -
 
+
 git clone https://github.com/nikita-skobov/create-bash-script ~/.local/share/create-bash-script
 git clone https://gitea.theedgeofrage.com/TheEdgeOfRage/rpn ~/.local/share/rpn
 git clone https://github.com/TheEdgeOfRage/bumblebee-status ~/.local/share/bumblebee-status
 
-mkdir -p ~/.config/zsh/plugins/
+local ohmyzsh_custom="~/.config/zsh/oh-my-zsh/custom"
 git clone https://github.com/TheEdgeOfRage/ohmyzsh ~/.config/zsh/oh-my-zsh
-
-git clone https://github.com/MichaelAquilina/zsh-you-should-use ~/.config/zsh/plugins/you-should-use
-git clone https://github.com/TheEdgeOfRage/zsh-autoswitch-virtualenv ~/.config/zsh/plugins/zsh-autoswitch-virtualenv
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ~/.config/zsh/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.config/zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/MichaelAquilina/zsh-you-should-use ${ohmyzsh_custom}/plugins/you-should-use
+git clone https://github.com/TheEdgeOfRage/zsh-autoswitch-virtualenv ${ohmyzsh_custom}/plugins/zsh-autoswitch-virtualenv
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ohmyzsh_custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ohmyzsh_custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ohmyzsh_custom}/plugins/zsh-syntax-highlighting
