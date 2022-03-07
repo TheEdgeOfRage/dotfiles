ccbeea1e3d705f3b6155502a1238538838d6927a
Pavle Portic <git@theedgeofrage.com>
2022-03-07 19:21:08 +0100

    Fix ohmyzsh var in init script

--- init.sh
+++ init.sh
@@ -11,8 +11,9 @@ git clone https://github.com/nikita-skobov/create-bash-script ~/.local/share/cre
 git clone https://gitea.theedgeofrage.com/TheEdgeOfRage/rpn ~/.local/share/rpn
 git clone https://github.com/TheEdgeOfRage/bumblebee-status ~/.local/share/bumblebee-status
 
-local ohmyzsh_custom="~/.config/zsh/oh-my-zsh/custom"
-git clone https://github.com/TheEdgeOfRage/ohmyzsh ~/.config/zsh/oh-my-zsh
+ohmyzsh_dir="~/.config/zsh/oh-my-zsh"
+ohmyzsh_custom="${ohmyzsh_dir}/custom"
+git clone https://github.com/TheEdgeOfRage/ohmyzsh ${ohmyzsh_dir}
 git clone https://github.com/MichaelAquilina/zsh-you-should-use ${ohmyzsh_custom}/plugins/you-should-use
 git clone https://github.com/TheEdgeOfRage/zsh-autoswitch-virtualenv ${ohmyzsh_custom}/plugins/zsh-autoswitch-virtualenv
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ohmyzsh_custom}/plugins/zsh-autosuggestions
