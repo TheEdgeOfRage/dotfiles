9fce0e5141cc9a4b44cbff2e6fe64e1d47d93c15
Pavle Portic <git@theedgeofrage.com>
2022-04-30 01:47:47 +0200

    Add boban theme to init script

--- init.sh
+++ init.sh
@@ -19,5 +19,6 @@ git clone https://github.com/TheEdgeOfRage/zsh-autoswitch-virtualenv ${ohmyzsh_c
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ohmyzsh_custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ohmyzsh_custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ohmyzsh_custom}/plugins/zsh-syntax-highlighting
+git clone https://gitea.theedgeofrage.com/TheEdgeOfRage/boban-zsh ${ohmyzsh_custom}/themes/boban
 
 sudo cp ./custom_colemak /usr/share/X11/xkb/symbols/custom
