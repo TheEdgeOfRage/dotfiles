73cbd87f6a4ce4130aedd657cdf30c12e7a2ab66
TheEdgeOfRage <git@theedgeofrage.com>
2024-07-24 10:11:52 +0200

    Add mapping to open hover LSP window to neovim

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -13,7 +13,7 @@ lsp_zero.on_attach(function(_, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 	vim.keymap.set("n", "<leader>vc", vim.lsp.buf.code_action, opts)
 	vim.keymap.set("n", "<leader>vr", vim.lsp.buf.rename, opts)
-
+	vim.keymap.set("n", "<leader>vh", vim.lsp.buf.hover, opts)
 	vim.keymap.set("n", "<leader>en", vim.diagnostic.goto_next, opts)
 	vim.keymap.set("n", "<leader>ep", vim.diagnostic.goto_prev, opts)
 	vim.keymap.set("n", "<leader>ei", vim.diagnostic.open_float, opts)
