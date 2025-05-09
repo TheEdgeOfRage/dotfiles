0cad2dd71268a87139b2017fd9e0eb86bf1a8974
TheEdgeOfRage <git@theedgeofrage.com>
2025-05-09 13:17:08 +0200

    Remove custom fold text and vertical line at 120

--- dot_config/nvim/lua/sets.lua
+++ dot_config/nvim/lua/sets.lua
@@ -8,7 +8,6 @@ vim.opt.laststatus = 2
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
-vim.opt.textwidth = 120
 vim.opt.colorcolumn = "+1"
 vim.opt.listchars = "tab:│ ,trail:~,extends:>,precedes:<"
 
@@ -16,36 +15,7 @@ vim.opt.foldenable = false
 vim.opt.foldlevel = 99
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
--- vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
-local function get_custom_foldtxt_suffix(foldstart)
-	local fold_suffix_str = string.format("  %s [%s lines]", "┉", vim.v.foldend - foldstart + 1)
-
-	return { fold_suffix_str, "Folded" }
-end
-
-local function get_custom_foldtext(foldtxt_suffix, foldstart)
-	local line = vim.api.nvim_buf_get_lines(0, foldstart - 1, foldstart, false)[1]
-
-	return {
-		{ line, "Normal" },
-		foldtxt_suffix,
-	}
-end
-
-_G.get_foldtext = function()
-	local foldstart = vim.v.foldstart
-	local ts_foldtxt = vim.treesitter.foldtext()
-	local foldtxt_suffix = get_custom_foldtxt_suffix(foldstart)
-
-	if type(ts_foldtxt) == "string" then
-		return get_custom_foldtext(foldtxt_suffix, foldstart)
-	else
-		table.insert(ts_foldtxt, foldtxt_suffix)
-		return ts_foldtxt
-	end
-end
-
-vim.opt.foldtext = "v:lua.get_foldtext()"
+vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
 
 vim.opt.termguicolors = true
 vim.opt.autochdir = true
