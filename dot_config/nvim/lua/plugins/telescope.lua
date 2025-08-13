7223a6daa81dfb38c1587604c99c98c52aeee6c4
TheEdgeOfRage <git@theedgeofrage.com>
2025-08-13 19:13:18 +0200

    Fix nvim telescope fzf native plugin

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			{
-				"nvim-telescope/telescope-fzf-native.nvim",
+				"jimrybarski/telescope-fzf-native.nvim",
 				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
 			},
 		},
