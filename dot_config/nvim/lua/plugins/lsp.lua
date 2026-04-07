392b801bcb9470e99071f4e15c704cd72483be50
TheEdgeOfRage <git@theedgeofrage.com>
2026-04-07 16:15:07 +0200

    Swap lsp enabled and config in nvim config

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -114,8 +114,8 @@ return {
 
 			for server, config in pairs(servers) do
 				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-				vim.lsp.enable(server)
 				vim.lsp.config(server, config)
+				vim.lsp.enable(server)
 			end
 		end,
 	},
